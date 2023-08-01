a534c8c6a5c5c36d6c166fbfa1e92cc14dfd2aa8
MatthewPotter20157 <125506925+MatthewPotter20157@users.noreply.github.com>
2023-08-01 14:20:31 +1200

    Section 1

--- Don't Crash/Assets/playerController.cs
+++ Don't Crash/Assets/playerController.cs
@@ -10,8 +10,8 @@ public class playerController : MonoBehaviour
     public float speed = 37;
 
 
-    Vector3 rotationRight = new Vector3(0, 50, 0);
-    Vector3 rotationLeft = new Vector3(0, -50, 0);
+    Vector3 rotationRight = new Vector3(0, 190, 0);
+    Vector3 rotationLeft = new Vector3(0, -190, 0);
 
     Vector3 forward = new Vector3(0, 0, 1);
     Vector3 backward = new Vector3(0, 0, -1);
@@ -21,23 +21,34 @@ public class playerController : MonoBehaviour
         if (Input.GetKey("w"))
         {
             transform.Translate(forward * speed * Time.deltaTime);
+            if (Input.GetKey("d"))
+            {
+                Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationRight);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationLeft);
+            }
         }
         if (Input.GetKey("s"))
         {
             transform.Translate(backward * speed * Time.deltaTime);
+            if (Input.GetKey("d"))
+            {
+                Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationRight);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationLeft);
+            }
         }
 
-        if (Input.GetKey("d"))
-        {
-            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotationRight);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotationLeft);
-        }
 
     }
 }
