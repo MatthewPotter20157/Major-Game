191f0f8eb347b1cb29f38afe68018cba3824a893
MatthewPotter20157 <125506925+MatthewPotter20157@users.noreply.github.com>
2023-08-15 14:21:07 +1200

    happy time

---
+++ Don't Crash/Assets/Scripts/playerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform car;
+    public float speed = 37;
+
+
+    Vector3 rotationRight = new Vector3(0, 190, 0);
+    Vector3 rotationLeft = new Vector3(0, -190, 0);
+
+    Vector3 forward = new Vector3(0, 0, 1);
+    Vector3 backward = new Vector3(0, 0, -1);
+    void Update()
+    {
+        Move();
+        
+    }
+
+    void Move()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(forward * speed * Time.deltaTime, Space.World);
+            if (Input.GetKey("d"))
+            {
+                
+                transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            }
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(backward * speed * Time.deltaTime);
+            if (Input.GetKey("d"))
+            {
+
+                transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            }
+        }
+
+    }
+}
