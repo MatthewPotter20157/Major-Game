88d38808414cd9a5b77e0df5733f129c11ae037b
MatthewPotter20157 <125506925+MatthewPotter20157@users.noreply.github.com>
2023-08-28 12:42:49 +1200

    final

--- Don't Crash/Assets/Scripts/BusStop.cs
+++ Don't Crash/Assets/Scripts/BusStop.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BusStop : MonoBehaviour
 {
+    public TextMeshProUGUI gameOver;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver.text = "";
+
+
     }
 
     // Update is called once per frame
@@ -16,6 +20,6 @@ public class BusStop : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-
+        gameOver.text = "gameOver";
     }
 }
--- Don't Crash/Assets/Scripts/RandomSpawner.cs
+++ Don't Crash/Assets/Scripts/RandomSpawner.cs
@@ -12,13 +12,12 @@ public class RandomSpawner : MonoBehaviour
     public float treeNumber;
     public float treeSpawnX;
     public float treeSpawnY;
-    // Click the "Instantiate!" button and a new `prefab` will be instantiated
-    // somewhere within -10.0 and 10.0 (inclusive) on the x-z plane
     void Start()
     {
         for(int i = 0; i < treeNumber; i++)
         {
             int treeIndex = Random.Range(0, trees.Length);
+            //spawn a sertain number of trees within the bounds set
             Vector3 treeSpawn = new Vector3(Random.Range(treeUpBound, treeDownBound), treeSpawnY, Random.Range(treeLeftBound, treeRightBound));
             Instantiate(trees[treeIndex], treeSpawn, trees[treeIndex].transform.rotation, GameObject.Find("TREES").transform);
         }
